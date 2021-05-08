a993dd5b6542fb2ef5af5e912e6d55ef89f08146
Dominik <e1625726@student.tuwien.ac.at>
2021-05-08 13:46:00 +0200

    v0.5

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import os, urllib
 import requests
 import argparse
 import time
-from ffmpeg import video
 import subprocess
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help='The url of the thread.')
 parser.add_argument("--watch", action='store_true', help='Watch for new posts and download them.')
-parser.add_argument("--nvidia", action='store_true', help='Use nvenc.')
+parser.add_argument("--encode", choices=['libx264', 'h264_nven', 'vp9'], default='libx264',
+                    help='libx264:mkv slow, h264_nven: mkv fast, vp9: webm')
+parser.add_argument("--out", default=None, help='Output path.')
 args = parser.parse_args()
 
-#TODO make these arguments, need to change the ffmpeg stuff for the format
 TMP_PATH = 'temp'
-OUT_PATH = 'out'
-OUT_FORMAT = 'mkv'
+OUT_FORMAT = {'libx264': 'mkv', 'h264_nven': 'mkv', 'vp9': 'webm'}[args.encode]
+
 
 class Chan:
-    def __init__(self, url):
+    def __init__(self, url, out):
         self.board = 'a'
         self.thread = 1
 
@@ -26,68 +26,86 @@ class Chan:
         url = url[:-1] if url.endswith('/') else url
         self.thread = url.split('/')[-1]
         self.board = url.split('/')[-3]
+        self.out = out if out is not None else self.thread
 
         if '4channel.org' in url or '4chan.org' in url:
             self.api = url + '.json'
-            raise NotImplementedError
+            self.start = self.fourchannel
         elif 'desuarchive' in url:
             self.api = f'https://desuarchive.org/_/api/chan/thread?board={self.board}&num={self.thread}'
+            self.start = self.desuarchive
         else:
-            raise Exception
+            raise NotImplementedError
 
         if not os.path.isdir(TMP_PATH):
             os.mkdir(TMP_PATH)
-        if not os.path.isdir(OUT_PATH):
-            os.mkdir(OUT_PATH)
+        if not os.path.isdir(self.out):
+            os.mkdir(self.out)
 
     def watch(self):
         while True:
-            self.download()
+            self.start()
             time.sleep(60)
 
-    def download(self):
+    def fourchannel(self):
+        data = requests.get(self.api).json()
+        errors = []
+        for p in tqdm(data['posts']):
+            if not 'filename' in p:
+                continue
+            url = f'http://i.4cdn.org/{self.board}/{str(p["tim"]) + p["ext"]}'
+            try:
+                self.download(p['filename'] + p['ext'], url, str(p['no']))
+            except BaseException:
+                errors.append(str(p['no']))
+        print(errors)
+
+    def desuarchive(self):
         data = requests.get(self.api).json()
         data = data[list(data)[0]]
+        data['posts'][data['op']['num']] = data['op']
         errors = []
-        enc = 'libx264' if not args.nvidia else 'h264_nvenc'
-
         for p in tqdm(data['posts'].values()):
             if p['media'] is None:
                 continue
-            name = p['media']['media_filename']
-            if '[sound' in name:
-                fname = name.split('[sound=')[0] + '_' + p["num"]
-                fname = fname[1:] if fname.startswith('_') else fname
-                surl = name.split('[sound=')[1].split('].')[0]
-                try:
-                    if not os.path.isfile(os.path.join(OUT_PATH, f'{fname}.{OUT_FORMAT}')):
-                        sound_tmp = os.path.join(TMP_PATH, fname+'_s.'+surl.split('.')[-1])
-                        pic_temp = os.path.join(TMP_PATH, fname+'_p.'+name.split('.')[-1])
-                        with open(sound_tmp, 'wb') as sound, open(pic_temp, 'wb') as pic:
-                            surl = 'https://' + surl if not surl.startswith('http') else surl
-                            sound.write(requests.get(urllib.parse.unquote(surl)).content)
-                            pic.write(requests.get(p['media']['media_link']).content)
+            try:
+                self.download(p['media']['media_filename'], p['media']['media_link'], p["num"])
+            except BaseException:
+                errors.append(str(p["num"]))
+        print(errors)
 
-                        if name.split('.')[-1] == 'webm':
-                            subprocess.run(['ffmpeg', '-stream_loop', '-1', '-i', f'{pic_temp}', '-i', f'{sound_tmp}',
-                                            '-strict' , '-2', '-c:v', enc, '-c:a', 'copy', '-shortest',
-                                            os.path.join(OUT_PATH, f'{fname}.{OUT_FORMAT}')])
-                        elif name.split('.')[-1] == 'gif':
-                            subprocess.run(['ffmpeg', '-ignore_loop', '0', '-i', f'{pic_temp}', '-i', f'{sound_tmp}',
-                                            '-strict' , '-2', '-c:v', enc, '-c:a', 'copy', '-shortest',
-                                            os.path.join(OUT_PATH, f'{fname}.{OUT_FORMAT}')])
-                        else:
-                            subprocess.run(['ffmpeg', '-loop', '0', '-i', f'{pic_temp}', '-i', f'{sound_tmp}',
-                                            '-strict', '-2', '-c:v', enc, '-vcodec', 'mpeg4', '-c:a', 'copy',
-                                            os.path.join(OUT_PATH, f'{fname}.{OUT_FORMAT}')])
-                except BaseException:
-                    errors.append(p["num"])
-        print('failed posts: ', errors)
+    def download(self, media_name, media_link, p_num):
+        if '[sound' in media_name:
+            fname = media_name.split('[sound=')[0] + '_' + p_num
+            fname = fname[1:] if fname.startswith('_') else fname
+            surl = media_name.split('[sound=')[1].split('].')[0]
+            if not os.path.isfile(os.path.join(self.out, f'{fname}.{OUT_FORMAT}')):
+                sound_tmp = os.path.join(TMP_PATH, fname+'_s.'+surl.split('.')[-1])
+                pic_temp = os.path.join(TMP_PATH, fname+'_p.'+media_name.split('.')[-1])
+                with open(sound_tmp, 'wb') as sound, open(pic_temp, 'wb') as pic:
+                    surl = 'https://' + surl if not surl.startswith('http') else surl
+                    sound.write(requests.get(urllib.parse.unquote(surl)).content)
+                    pic.write(requests.get(media_link).content)
+
+                if media_name.split('.')[-1] == 'webm':
+                    subprocess.run(['ffmpeg', '-stream_loop', '-1', '-i', f'{pic_temp}', '-i', f'{sound_tmp}',
+                                    '-strict' , '-2', '-c:v', args.encode, '-c:a', 'copy', '-shortest',
+                                    os.path.join(self.out, f'{fname}.{OUT_FORMAT}')])
+                elif media_name.split('.')[-1] == 'gif':
+                    subprocess.run(['ffmpeg', '-ignore_loop', '0', '-i', f'{pic_temp}', '-i', f'{sound_tmp}',
+                                    '-strict' , '-2', '-c:v', args.encode, '-c:a', 'copy', '-shortest',
+                                    os.path.join(self.out, f'{fname}.{OUT_FORMAT}')])
+                else:
+                    subprocess.run(['ffmpeg', '-loop', '0', '-i', f'{pic_temp}', '-i', f'{sound_tmp}',
+                                    '-strict', '-2', '-c:v', args.encode, '-vcodec', 'mpeg4', '-c:a', 'copy',
+                                    os.path.join(self.out, f'{fname}.{OUT_FORMAT}')])
 
 
 if __name__ == '__main__':
-    c = Chan(args.url)
+    c = Chan(args.url, args.out)
+    if OUT_FORMAT == 'webm':
+        raise NotImplementedError
     if not args.watch:
-        c.download()
+        c.start()
     else:
         c.watch()
